59f67cdbc9879c8a6335c356bbd807916988966d
stefania11 <druga.stefania@gmail.com>
2023-03-10 14:47:14 -0800

    add new line

--- agents/haikuart/main.py
+++ agents/haikuart/main.py
@@ -5,28 +5,28 @@
 import fixieai
 
 SD_PROMPT_SUFFIX = (
-    "image, 2:3, elegant, highly detailed, centered, digital painting,"
+    "image, 2:3, elegant, highly detailed, centered, digital painting"
 )
 
 BASE_PROMPT = """I am intelligent agent that generates a haiku based on a prompt and generates images that go with it."""
 
 FEW_SHOTS = f"""
-Q: Write a haiku about clouds and generate an image description that goes with it \
-Thought: I need to generate a short haiku about clouds and generate an image description that goes with it \
-Ask Agent[stable_diffusion]:  A serene blue sky with fluffy white clouds drifting lazily by, {SD_PROMPT_SUFFIX} \
-Agent[stable_diffusion] says:[image1]
-A:[image1]
+Q: Write a haiku about clouds and generate an image description that goes with it
+Thought: I need to generate a short haiku about clouds and generate an image description that goes with it
+Ask Agent[stable_diffusion]:  A serene blue sky with fluffy white clouds drifting lazily by, {SD_PROMPT_SUFFIX}
+Agent[stable_diffusion] says: [image1]
+A: [image1] \n
 Clouds in the sky high
 Fluffy pillows floating by
 Dreamy thoughts up high
 
 
 Q: Write a haiku about rain
-Thought: I need to generate a short haiku about rain and generate an image description that goes with it \
+Thought: I need to generate a short haiku about rain and generate an image description that goes with it
 Ask Agent[stable_diffusion]: A rainy day with raindrops falling on a rooftop, creating a pattern of \
 concentric circles, {SD_PROMPT_SUFFIX}
-Agent[stable_diffusion] says:[image1]
-A:[image1]
+Agent[stable_diffusion] says: [image1]
+A: [image1] \n
 Raindrops on the roof,
 A soothing rhythm of life,
 Renewing the earth.
