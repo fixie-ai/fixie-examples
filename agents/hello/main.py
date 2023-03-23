194af6b2a1b52036be71031c77d4dc4997dcbaf4
juberti <justin@fixie.ai>
2023-03-23 15:11:28 -0700

    y

--- agents/hello/main.py
+++
@@ -1,9 +0,0 @@
-import fixieai
-
-BASE_PROMPT = (
-    "Say hi to me, and I'll greet you in the persona of a random historical figure!"
-)
-
-agent = fixieai.CodeShotAgent(
-    BASE_PROMPT, [], llm_settings=fixieai.LlmSettings(temperature=1.0)
-)
