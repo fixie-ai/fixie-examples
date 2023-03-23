46e231074ba6d14e91cfcc87e9251b416dc4ba2c
juberti <justin@fixie.ai>
2023-03-22 18:19:48 -0700

    Add follow-up few-shots

--- agents/chatembed/main.py
+++ agents/chatembed/main.py
@@ -1,7 +1,9 @@
 import fixieai
 
 BASE_PROMPT = (
-    """Hi, I'm a helpful chatbot who can answer questions from an uploaded file."""
+    "Hi, I'm a helpful chatbot who can answer questions from an uploaded file. "
+    "I do Ask Func[fixie_query_embed] to extract answers, and when I do so, I always append the previously mentioned embed reference, e.g., #text1. "
+    "If no embed reference exists, I tell the user they need to first upload a document."
 )
 
 FEW_SHOTS = """
@@ -10,10 +12,18 @@ Ask Func[fixie_query_embed]: When did the thing happen? #text1
 Func[fixie_query_embed] says: It happened on March 1, 2023
 A: The thing happened on March 1, 2023.
 
+Q: When did the thing happen? #text1
+Ask Func[fixie_query_embed]: When did the thing happen? #text1
+Func[fixie_query_embed] says: It happened on March 1, 2023
+A: The thing happened on March 1, 2023.
 Q: What was the thing?
 Ask Func[fixie_query_embed]: What was the thing? #text1
-Func[fixie_query_embed] says: The thing was a object.
-A: The thing was a object.
+Func[fixie_query_embed] says: The thing was an event.
+A: The thing was an event.
+Q: Why did the thing occur?
+Ask Func[fixie_query_embed]: Why did the thing occur? #text1
+Func[fixie_query_embed] says: The thing occurred for a reason.
+A: The thing occured for a reason.
 """
 
 agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS, conversational=True)
