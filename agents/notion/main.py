92c93183f528e6f1d7ac264e57d7b9b184f25bd4
Matt Welsh <matt@fixie.ai>
2023-04-01 15:52:46 -0700

    Testing this.

--- agents/notion/main.py
+++ agents/notion/main.py
@@ -3,41 +3,44 @@ import fixieai
 
 from notion_client import Client
 
+NOTION_DATABASE_ID = "b264de3998384f839245bd54faa40d9c"
+
+BASE_PROMPT = """I am an agent that answers questions about a set of web pages, stored in a Notion database."""
+
+FEW_SHOTS = """
+Q: Which articles are about dark energy?
+Ask Func[fixie_query_corpus]: Which articles are about dark energy?
+Func[fixie_query_corpus] says: The following article is about dark energy: \
+"Dark Energy Spectroscopic Instrument (DESI) Creates Largest 3D Map of the Cosmos".
+A: The following article is about dark energy: \
+"Dark Energy Spectroscopic Instrument (DESI) Creates Largest 3D Map of the Cosmos".
+Q: Summarize it for me.
+Ask Func[fixie_query_corpus]: Summarize it for me.
+Func[fixie_query_corpus] says: The article "Dark Energy Spectroscopic Instrument (DESI) Creates \
+Largest 3D Map of the Cosmos" is about dark energy. It was written by Adam Becker, \
+and discusses how DESI has already mapped out more galaxies than all previous 3D \
+surveys combined.
+A: The article "Dark Energy Spectroscopic Instrument (DESI) Creates \
+Largest 3D Map of the Cosmos" is about dark energy. It was written by Adam Becker, \
+and discusses how DESI has already mapped out more galaxies than all previous 3D \
+surveys combined.
+"""
+
+# Open the Notion database and read all of the URLs in it.
 with open("notion-key.txt", "r") as notion_key_file:
     os.environ["NOTION_TOKEN"] = notion_key_file.read().strip()
 
-notion = Client(auth=os.environ["NOTION_TOKEN"])
-
-NOTION_DATABASE_ID = "b264de3998384f839245bd54faa40d9c"
-
 print(f"Loading databse {NOTION_DATABASE_ID}...")
+notion = Client(auth=os.environ["NOTION_TOKEN"])
 pages = notion.databases.query(database_id=NOTION_DATABASE_ID)
 print(f"Found {len(pages['results'])} pages in database {NOTION_DATABASE_ID}.")
+
+URLS = []
 for page in pages["results"]:
-    print("---------------------")
-    page_title = page["properties"]["Title"]["title"][0]["plain_text"]
     page_url = page["properties"]["URL"]["url"]
-    print(page_url + " - " + page_title)
+    print(f"   {page_url}")
+    URLS.append(page_url)
 
 
-BASE_PROMPT = """I am an agent that answers questions from a knowledge base stored in Notion."""
-
-FEW_SHOTS = """
-Q: How do I get started writing a Fixie agent?
-Ask Func[fixie_query_corpus]: How do I get started writing a Fixie agent?
-Func[fixie_query_corpus] says: Use the Fixie CLI to create a new agent using `fixie init`.
-A: Use the Fixie CLI to create a new agent using `fixie init`.
-
-Q: What example agents show how to use images?
-Ask Func[fixie_query_corpus]: What example agents have the tag "images"?
-Func[fixie_query_corpus] says: The chart and posters agents are both good examples of how to \
-use images.
-A: The chart and posters agents are both good examples of how to use images.
-"""
-
-# URLS = [
-#     "https://docs.fixie.ai/*",
-# ]
-
-# CORPORA = [fixieai.DocumentCorpus(urls=URLS)]
-# agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS, CORPORA)
+CORPORA = [fixieai.DocumentCorpus(urls=URLS)]
+agent = fixieai.CodeShotAgent(BASE_PROMPT, FEW_SHOTS, CORPORA)
