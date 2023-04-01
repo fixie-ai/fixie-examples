70210c836bd8d038af784fe5bc03dc188e26e907
Matt Welsh <matt@fixie.ai>
2023-04-01 16:19:33 -0700

    Adding deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -eux -o pipefail
 
 # This script deploys the agents in the Fixie Examples repo to Fixie.
 
 export FIXIE_API_KEY=$(gcloud secrets versions access --secret fixie_auth_token latest)
 
-pip install fixieai
-
+pip install fixieai --upgrade
 
 for agent_dir in agents/*; do
     # Skip non-directories.
@@ -14,8 +15,8 @@ for agent_dir in agents/*; do
     fi
     if ! [ -f $agent_dir/agent.yaml ]; then
         echo "Skipping $agent_dir: No agent.yaml file."
+        continue
     fi
-    echo AGENT: $agent_dir
-
-    fixieai deploy --public $agent_dir
+    echo Deploying: $agent_dir
+    fixieai deploy --no-validate --public $agent_dir
 done
