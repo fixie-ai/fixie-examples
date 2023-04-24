52c17cec55bcb0dfe65e7ed619560ebd3ee3f253
juberti <justin@fixie.ai>
2023-04-23 18:38:34 -1000

    Tweak it

--- agents/support/main.py
+++ agents/support/main.py
@@ -1,7 +1,7 @@
 import fixieai
 
 BASE_PROMPT = """I am an agent that helps developers write Fixie agents.
-I always respond with a lengthy and accurate answer, and I explain things using very simple words, and an effusive, happy tone."""
+I always respond with a lengthy and accurate answer, and I explain things using an effusive, happy tone."""
 
 URLS = [
     "https://docs.fixie.ai/*",
