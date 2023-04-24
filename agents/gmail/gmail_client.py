a0ec1d4be8e212bddc4885cb8c67e32853c90e5d
Farzad Abdolhosseini <farzad@fixie.ai>
2023-04-24 15:02:31 -0700

    added a very basic gmail retrieval agent

---
+++ agents/gmail/gmail_client.py
@@ -0,0 +1,174 @@
+"""A (very) simple client for the Gmail API."""
+
+import base64
+import dataclasses
+import datetime
+import logging
+from typing import List, Optional
+
+import googleapiclient.discovery
+import googleapiclient.errors
+from bs4 import BeautifulSoup
+from google.oauth2 import credentials as gcreds
+
+logger = logging.getLogger(__name__)
+
+DATE_PRINT_FORMAT: str = "%A %B %d %Y %H:%M"
+
+
+class GMailClient:
+    """A wrapper around GMail API to return clean objects for consumption.
+
+    The API doc can be found here:
+        https://developers.google.com/gmail/api/guides
+        https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list
+    """
+
+    def __init__(self, access_token: str):
+        self.client = googleapiclient.discovery.build(
+            "gmail",
+            "v1",
+            credentials=gcreds.Credentials(token=access_token),
+        )
+
+    def get_message(self, message_id: str, format="full") -> "Message":
+        """Get a message by its ID."""
+        raw_message = (
+            self.client.users()
+            .messages()
+            .get(
+                userId="me",
+                format=format,
+                id=message_id,
+            )
+        ).execute()
+        return Message.from_raw_message(raw_message)
+
+    def list(
+        self,
+        search_query: str = "",
+        limit: int = 10,
+    ) -> List["Message"]:
+        """List messages in the inbox.
+
+        The API doc can be found here:
+            https://developers.google.com/gmail/api/reference/rest/v1/users.messages/list
+        """
+        query = (
+            self.client.users()
+            .messages()
+            .list(
+                userId="me",
+                q=search_query,
+                maxResults=limit,
+            )
+        )
+
+        raw_messages = query.execute()
+        raw_messages = raw_messages.get("messages", [])
+
+        # TODO: return token for pagination?
+        # TODO: parallelize requests?
+        return [self.get_message(mini_msg["id"]) for mini_msg in raw_messages]
+
+
+@dataclasses.dataclass
+class Message:
+    id: str
+    threadId: str
+    snippet: Optional[str] = None
+    date: Optional[datetime.datetime] = None
+    body: Optional[str] = None
+    # The following are from payload.headers:
+    title: Optional[str] = None
+    author: Optional[str] = None
+    recipients: Optional[List[str]] = None
+
+    @classmethod
+    def from_raw_message(cls, d: dict) -> "Message":
+        """Create a Message from a raw message dict."""
+        msg = cls(id=d["id"], threadId=d["threadId"], snippet=d.get("snippet", None))
+
+        if "internalDate" in d:
+            msg.date = datetime.datetime.fromtimestamp(int(d["internalDate"]) / 1000)
+
+        payload = d.get("payload", None)
+        if payload:
+            # extract headers
+            headers = payload.get("headers", [])
+            for header in headers:
+                if header["name"] == "Subject":
+                    msg.title = header["value"]
+                elif header["name"] == "From":
+                    msg.author = header["value"]
+                elif header["name"] == "To":
+                    msg.recipients = header["value"].split(", ")
+
+            # decode body
+            if "parts" in payload:
+                for part in payload["parts"]:
+                    if part["mimeType"] == "text/plain":
+                        msg.body = _body_to_human_readable(part["body"]["data"])
+            elif "body" in payload and "data" in payload["body"]:
+                msg.body = _body_to_human_readable(payload["body"]["data"])
+            else:
+                logger.warning(f"No body found in message {msg.id}")
+
+        return msg
+
+    def to_str(
+        self,
+        sep: str = "  ",
+        include_body: bool = False,
+        include_snippet: bool = False,
+        include_recipients: bool = False,
+        end: str = " <END>",
+        tz: Optional[datetime.timezone] = None,
+    ) -> str:
+        """Convert the message to a human readable representation."""
+        rep = ""
+        if self.title:
+            rep += f"{self.title}{sep}"
+        if self.author:
+            rep += f"From: {self.author}{sep}"
+        if include_recipients and self.recipients:
+            rep += f"To: {', '.join(self.recipients)}{sep}"
+        if self.date:
+            date = self.date.astimezone(tz) if tz else self.date
+            rep += f"Date: {date.strftime(DATE_PRINT_FORMAT)}{sep}"
+
+        if include_snippet and self.snippet:
+            rep += f"Snippet: {self.snippet}{sep}"
+        if include_body and self.body:
+            if sep == "\n":
+                # Add extra new-line for readability
+                rep += f"{sep}{self.body}{sep}"
+            else:
+                rep += f"Body: {self.body}{sep}"
+
+        rep = rep.strip() + end
+
+        return rep
+
+    def short_str(self, tz: Optional[datetime.timezone] = None) -> str:
+        """A single-line string representation of the message."""
+        return self.to_str(sep="  ", tz=tz)
+
+    def long_str(self, tz: Optional[datetime.timezone] = None) -> str:
+        """A multi-line string representation of the message."""
+        return self.to_str(
+            sep="\n",
+            include_body=True,
+            include_recipients=True,
+            tz=tz,
+        )
+
+
+def _body_to_human_readable(body: str) -> str:
+    """Convert a base64-encoded body to human-readable text.
+    If the body is HTML, we will extract only the text from it.
+    """
+    body = base64.urlsafe_b64decode(body).decode("utf-8")
+    # If the body is HTML, we want to extract the text from it.
+    body = BeautifulSoup(body, "html.parser").get_text(separator="\n")
+    return body.strip()
---
+++ agents/gmail/main.py
@@ -0,0 +1,163 @@
+"""GMail` agent example!
+
+It can:
+* Check your email.
+* Search or filter your email.
+"""
+
+import sys
+import base64
+import json
+import datetime
+
+import fixieai
+import gmail_client
+
+try:
+    oauth_params = fixieai.OAuthParams.from_client_secrets_file(
+        "gcp-oauth-secrets.json",
+        ["https://www.googleapis.com/auth/gmail.readonly"],
+    )
+except FileNotFoundError:
+    print(
+        "gcp-oauth-secrets.json was not found! You'll need to generate your own oauth "
+        "to deploy this agent. For more info, follow the 4-step instructions here: "
+        "https://developers.google.com/identity/protocols/oauth2/javascript-implicit-flow#creatingcred"
+    )
+    sys.exit(4)
+
+USER_TIMEZONE = datetime.timezone(datetime.timedelta(hours=-8))
+EMBED_NAME = "email"
+
+BASE_PROMPT = """I am an intelligent email agent that can check your google mail (Gmail) account for messages.
+The messages will end with <END> to indicate the end of the message, \
+but I will not print it out in my answer. \
+When reading an email, I will print it out faithfully without any changes or making up words.
+
+By default I limit my responses to maximum 10 messages unless instructed otherwise.
+
+search_query can use any of the following operators:
+- is:unread/read
+- in:inbox/sent/starred/trash/spam
+- from:email/name
+- to:email/name
+- subject:string
+- General search (body/title/author/etc): string
+- Search for an exact match: " "
+"""
+
+FEW_SHOTS = """
+Q: What are my latest emails?
+Thought: I need to get a list of emails from the user's inbox. \
+By default the emails are returned from last to first. \
+I will limit the number of emails to 10.
+Ask Func[list]: { "limit": 10, "search_query": "in:inbox" }
+Func[list] says: https://accounts.google.com/o/oauth2/auth?foo=bar
+Thought: I need to pass the auth url to the user.
+A: I don't have access to your calendar. Please [authorize](https://accounts.google.com/o/oauth2/auth?foo=bar).
+
+Q: What new emails do I have?
+Thought: I need to get a list of unread emails from the user's inbox. \
+I will limit the number of emails to 10.
+Ask Func[list]: { "limit": 10, "search_query": "is:unread in:inbox" }
+Func[list] says: You have 2 unread emails in your inbox:
+1. [#doc10] Welcome to Fixie!  From: hello@fixie.ai  To: me@mail.com
+2. [#doc12] You wouldn't believe what I just heard about XYZ  From news@journal.io
+A: You have 2 unread emails:
+1. Welcome to Fixie!
+2. You wouldn't believe what ...
+Q: read me the email from Fixie.
+Thought: I need to retrieve the email from hello@fixie.ai by its ID.
+Ask Func[get_message]: { "message_id": "#doc10" }
+Func[get_message] says: Title: Welcome to Fixie!
+From: hello@fixie.ai
+To: me@mail.com
+Date: Dec 14, 2020
+Hi there! Welcome to Fixie! We are so excited to have you on board. \
+We hope you enjoy using Fixie as much as we enjoy building it. <END>
+Thought: I need to repeat back this email.
+A: Title: Welcome to Fixie!
+From: hello@fixie.ai
+To: me@mail.com
+Date: Dec 14, 2020
+Hi there! Welcome to Fixie! We are so excited to have you on board. \
+We hope you enjoy using Fixie as much as we enjoy building it.
+
+Q: Get me a list of emails from Jason.
+Thought: I need to get a list of emails from Jason.
+Ask Func[list]: { "limit": 10, "search_query": "from:Jason in:inbox" }
+Func[list] says: You have 3 emails from Jason in your inbox:
+1. [#doc8] We Are in Trouble  From: Jason jason_dude@gmail.com  To: Myself me@email.com  Date: Dec 14, 2020
+2. [#doc10] An update regarding the CRA strike and filing your taxes  From: Jason jason_dude@gmail.com  Date: Apr 12, 2019
+3. [#doc7] What a doozy!  From: Jason not_jason@not_jason.com  Date: March 8, 2019
+A: You have 3 emails from Jason in your inbox:
+1. We Are in Trouble (From: Jason on Dec 14, 2020)
+2. An update regarding the CRA strike and filing your taxes (From Jason on Apr 12, 2019)
+3. What a doozy! (From: Not Jason on March 8, 2019)
+Q: Read me the third email?
+Thought: I need to retrieve the third email from the list by its ID.
+Ask Func[get_message]: { "message_id": "#doc7" }
+Func[get_message] says: Title: What a doozy!
+From: Jason not_jason@not_jason.com
+Date: Jan 1, 2021
+I just wanted to acknowledge what a tough time this is for everyone, but I'm confident we can find a solution together.
+Let me know if you want to chat! <END>
+Thought: I need to repeat back this email.
+A: Title: What a doozy!
+From: Jason not_jason@not_jason.com
+Date: Jan 1, 2021
+I just wanted to acknowledge what a tough time this is for everyone, but I'm confident we can find a solution together.
+Let me know if you want to chat!
+"""
+agent = fixieai.CodeShotAgent(
+    BASE_PROMPT, FEW_SHOTS, conversational=True, oauth_params=oauth_params
+)
+
+
+@agent.register_func
+def list(
+    query: fixieai.Message, oauth_handler: fixieai.OAuthHandler
+) -> fixieai.Message:
+    """List emails in user's inbox."""
+    user_token = oauth_handler.user_token()
+    if user_token is None:
+        return oauth_handler.get_authorization_url() or ""
+
+    list_args = json.loads(query.text)
+    client = gmail_client.GMailClient(user_token)
+    # TODO: type-checking for list_args?
+    messages = client.list(**list_args)
+    if messages:
+        embeds = [
+            fixieai.Embed(
+                content_type="text/plain",
+                uri=content_to_uri(msg.long_str(tz=USER_TIMEZONE)),
+            )
+            for msg in messages
+        ]
+        message_lines = [
+            f"{i+1}. #{EMBED_NAME}{i} " + msg.short_str(tz=USER_TIMEZONE)
+            for i, msg in enumerate(messages)
+        ]
+
+        # Note not to add "#" at the start of embeds_dict keys.
+        embeds_dict = {f"{EMBED_NAME}{i}": embed for i, embed in enumerate(embeds)}
+
+        message_text = f"You have {len(messages)} unread emails:\n" + "\n".join(message_lines)
+        return fixieai.Message(text=message_text, embeds=embeds_dict)
+    else:
+        return fixieai.Message(text="No emails matched the filter.")
+
+
+@agent.register_func
+def get_message(query: fixieai.Message) -> str:
+    """Get a specific message."""
+    if not query.embeds:
+        return "You need to specify the embed ID of the message you want to read."
+
+    email_embed = next(iter(query.embeds.values()))
+    return fixieai.Message(text=email_embed.text)
+
+
+def content_to_uri(content: str) -> str:
+    return f"data:base64,{base64.b64encode(content.encode('utf-8')).decode('utf-8')}"
