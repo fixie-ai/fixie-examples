da50e389bcf03cf01308ea8ea2f8d20022051664
Matt Welsh <matt@fixie.ai>
2023-05-01 22:40:58 -0700

    Clean up.

--- agents/silicon_valley/main.py
+++ agents/silicon_valley/main.py
@@ -7,21 +7,6 @@ BASE_PROMPT = (
     "reference."
 )
 
-# FEW_SHOTS = """
-# Q: Who was Gilfoyle played by?
-# Ask Func[fixie_query_corpus]: Who was Gilfoyle played by?
-# Func[fixie_query_corpus] says: Gilfoyle was played by Martin Starr.
-# A: Gilfoyle was played by Martin Starr.
-#
-# Q: In which season did Jian-Yang make the Hot Dog app?
-# Ask Func[fixie_query_corpus]: In which season did Jian-Yang make the Hot Dog app?
-# Func[fixie_query_corpus] says: Jian-Yang made the SeeFood app, which only recognized pictures of hot dogs, in Season 4, Episode 4.
-# A: Jian-Yang made the SeeFood app, which only recognized pictures of hot dogs, in Season 4, Episode 4.
-# Q: How did it go?
-# Ask Func[fixie_query_corpus]: How did Jing-Yang's Hot Dog app go?
-# Func[fixie_query_corpus] says: Jian-Yang's Hot Dog app was acquired by Periscope.
-# A: Jian-Yang's Hot Dog app was acquired by Periscope.
-# """
 FEW_SHOTS = []
 
 URLS = [
