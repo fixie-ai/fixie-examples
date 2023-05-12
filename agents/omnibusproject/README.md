21fe7c1cdfc8c8354781adff1710a90a1ddc39ac
Matt Welsh <matt@fixie.ai>
2023-05-11 21:32:44 -0700

    Omnibus Project demo agent (#52)
    This PR adds an agent that answers questions about the Omnibus Project
    podcast:
    
    https://www.omnibusproject.com/
    
    The README file explains how everything works and how to deploy it.
    
    Demo here:
    
    https://app.fixie.ai/agents/mdw/omnibusproject
    
    ---------
    
    Co-authored-by: Nick Heiner <NickHeiner@users.noreply.github.com>

---
+++ agents/omnibusproject/README.md
@@ -0,0 +1,90 @@
+# The Omnibus Project
+
+This is an Agent that answers questions about the hit podcast, 
+[The Omnibus Project](https://www.omnibusproject.com/), by Ken Jennings and John Roderick.
+This podcast is a compendium of arcane trivia and esoteric knowledge, and is a perfect
+demonstration use case for Fixie agents being able to take what is a complex,
+meandering dialogue between two people and turn it into a useful knowledge base.
+
+The agent works by downloading the audio for each episode, transcribing it using the
+Deepgram speech-to-text AI model, and creating a Fixie agent using the resulting
+transcripts.
+
+[Try it yourself!](https://app.fixie.ai/agents/mdw/omnibusproject)
+
+Try asking questions like:
+
+* Why does John pronounce "England" as "Englang"?
+* Summarize the episode about Rhinos in Europe
+* Who was Magic Alex?
+
+## Running the agent yourself
+
+## Downloading and transcribing the podcast audio
+
+The code in `downloader` deals with downloading and transcribing the audio for all of the
+Omnibus Project episodes. It checks the RSS feed for the podcast, downloads any new episodes, 
+uses the Deepgram speech-to-text model to transcribe the audio, and stores the results in a
+Google Cloud Storage bucket.
+
+To use this tool, you need to do the following.
+
+First, create a Google Cloud Storage project and GCS bucket. Edit `downloader/main.py` and
+change the `GCS_BUCKET` variable to the name of your bucket.
+
+For now, the contents of the bucket need to be world-readable for Fixie to be able to fetch
+the transcripts and answer questions. You can do this by running:
+
+```
+$ gsutil defacl set public-read gs://<your bucket name>
+```
+
+Next, create a Service Account with `storage.read` and `storage.write` permissions on this bucket, 
+and download the JSON key file.
+
+Create an account on [Deepgram](https://www.deepgram.com/), and create an API key.
+
+Create the file `.env` in the `downloader` directory, and add the following lines:
+
+```
+GOOGLE_APPLICATION_CREDENTIALS=<path to the JSON key file for your service account>
+DEEPGRAM_API_KEY=<your Deepgram API key>
+```
+
+You can then run:
+
+```
+$ poetry install
+$ poetry run ./downloader/main.py
+```
+
+This will download each episode, transcribe it, and write three files per episode to the
+`GCS_BUCKET` bucket: 
+* `NNN.mp3` - the MP3 file of the episode audio
+* `NNN.json` - the raw JSON result from the Deepgram transcription
+* `NNN.txt` - the full text extracted from the Deepgram result
+where `NNN` is the episode number.
+
+This process takes about 10 seconds per episode.
+
+The script will write the file `episodes.yaml` to the local directory, which contains a list
+of all of the `.txt` files. The Fixie agent code reads this file on startup to populate
+the list of URLs it will use to answer questions.
+
+Using the Deepgram Nova model, it will cost about $0.25 per episode
+to transcribe; as of May 2023 there are 546 episodes, so the $200
+credit for new accounts should be enough to transcribe them all.
+
+## Deploying the Agent
+
+After running the `downloader` and populating the GCS bucket, you can deploy the agent
+using:
+
+```
+$ fixie deploy
+```
+
+from the `agents/omnibusproject` directory. This will create a new Fixie agent called
+`omnibusproject`. It will take a while (maybe an hour or so) for the Agent to fully index
+all of the episode transcripts, so be patient.
+
---
+++ agents/omnibusproject/downloader/downloader/main.py
@@ -0,0 +1,179 @@
+#!/usr/bin/env python3
+
+# This tool downloads all episodes of the Omnibus Project podcast, and uses
+# Deepgram to generate transcripts for each episode.
+
+import datetime
+import json
+import os
+import re
+import tempfile
+
+import click
+import feedparser
+import requests
+import yaml
+from deepgram import Deepgram
+from google.cloud import storage
+from rich.console import Console
+
+DEEPGRAM_API_KEY = os.getenv("DEEPGRAM_API_KEY")
+if not DEEPGRAM_API_KEY:
+    raise ValueError("Please set the DEEPGRAM_API_KEY environment variable")
+
+# The default RSS feed to process. This is for the Omnibus Project podcast.
+RSS_FEED = "https://feeds.fireside.fm/omnibus/rss"
+
+# The default GCS bucket to upload data (audio and transcripts) to.
+GCS_BUCKET = "mdw-omnibus-project-audio"
+
+console = Console()
+
+
+class Episode:
+    """Represents a single podcast episode.
+
+    Args:
+        rss_entry: The RSS entry for the episode.
+        gcs_bucket: The GCS bucket to upload data to.
+    """
+
+    def __init__(self, rss_entry, gcs_bucket, skip_processing=False):
+        self._entry = rss_entry
+        self._gcs_bucket = gcs_bucket
+        self._skip_processing = skip_processing
+
+        self._title = rss_entry.title
+        self._mp3_url = self._entry.enclosures[0].href
+        m = re.match("^Episode (\d+)", self._title)
+        if not m:
+            raise ValueError(f"Could not parse episode number from {self._title}")
+        self._episode = m.group(1)
+        self._mp3_filename = f"{self._episode}.mp3"
+        self._transcript_filename = f"{self._episode}.json"
+        self._fulltext_filename = f"{self._episode}.txt"
+
+        self._storage_client = storage.Client()
+        self._bucket = self._storage_client.bucket(self._gcs_bucket)
+        self._mp3_blob = self._bucket.blob(self._mp3_filename)
+        self._transcript_blob = self._bucket.blob(self._transcript_filename)
+        self._fulltext_blob = self._bucket.blob(self._fulltext_filename)
+
+    def process(self):
+        """Process this episode and return a signed URL for the full text."""
+        if not self._skip_processing:
+            self.download_mp3()
+            self.transcribe()
+            self.extract_text()
+        return f"https://storage.googleapis.com/{self._gcs_bucket}/{self._fulltext_filename}"
+
+    def upload_to_gcs(self, localfile, destfile):
+        """Upload the given file to GCS."""
+        storage_client = storage.Client()
+        bucket = storage_client.bucket(self._gcs_bucket)
+        blob = bucket.blob(destfile)
+        print(f"Uploading {localfile} to {self._gcs_bucket}/{destfile}...")
+        with console.status(
+            f"Uploading {localfile} to {self._gcs_bucket}/{destfile}..."
+        ):
+            blob.upload_from_filename(localfile)
+
+    def download_mp3(self):
+        """Download the MP3 for the episode and store in GCS."""
+        if self._mp3_blob.exists():
+            console.print(
+                f"[yellow]Skipping download of {self._title} (already downloaded)"
+            )
+            return
+        with tempfile.NamedTemporaryFile(suffix=".mp3") as tmpfile:
+            with console.status(f"Downloading {self._title}..."):
+                r = requests.get(self._mp3_url, allow_redirects=True, stream=True)
+                with open(tmpfile.name, "wb") as outfile:
+                    for chunk in r.iter_content(chunk_size=128):
+                        outfile.write(chunk)
+            self.upload_to_gcs(tmpfile.name, self._mp3_filename)
+
+    def signed_url(self, blob):
+        """Return a signed URL for the given GCS blob."""
+        blob_url = blob.generate_signed_url(
+            version="v4", expiration=datetime.timedelta(seconds=86400), method="GET"
+        )
+        return blob_url
+
+    def transcribe(self):
+        """Transcribe the episode and store the transcript in GCS."""
+        if self._transcript_blob.exists():
+            console.print(
+                f"[yellow]Skipping transcription of {self._title} (already transcribed)"
+            )
+            return
+
+        with console.status(f"Transcribing {self._title}..."):
+            dg = Deepgram(DEEPGRAM_API_KEY)
+            MIMETYPE = "mp3"
+            options = {"punctuate": True, "model": "general", "tier": "nova"}
+            blob_url = self.signed_url(self._mp3_blob)
+            console.print(f"Transcribing {self._title} from {blob_url}...")
+            source = {"url": blob_url}
+            res = dg.transcription.sync_prerecorded(source, options)
+        with tempfile.NamedTemporaryFile(suffix=".json") as tmpfile:
+            with open(tmpfile.name, "w") as transcript:
+                json.dump(res, transcript)
+            self.upload_to_gcs(tmpfile.name, self._transcript_filename)
+        console.print(f"Saved transcript to {self._transcript_filename}")
+
+    def extract_text(self):
+        """Extract the raw text from the transcript and store in GCS."""
+        if self._fulltext_blob.exists():
+            console.print(
+                f"[yellow]Skipping extraction of {self._title} (already extracted)"
+            )
+            return
+        if not self._transcript_blob.exists():
+            raise ValueError(f"Transcript {self._transcript_filename} does not exist")
+        with console.status(f"Extracting transcript from {self._title}..."):
+            transcript_url = self.signed_url(self._transcript_blob)
+            resp = requests.get(transcript_url)
+            resp.raise_for_status()
+            transcript = resp.json()
+            full_text = (
+                transcript.get("results", {})
+                .get("channels", [{}])[0]
+                .get("alternatives", [{}])[0]
+                .get("transcript", "")
+                # Break each sentence onto its own line. The transcripts we get
+                # from Deepgram don't do this for us, and having everything on one
+                # single long line doesn't work well with our chunking logic.
+                .replace(". ", ".\n")
+            )
+
+        with tempfile.NamedTemporaryFile(suffix=".txt") as tmpfile:
+            with open(tmpfile.name, "w") as outfile:
+                outfile.write(full_text)
+            self.upload_to_gcs(tmpfile.name, self._episode + ".txt")
+
+
+@click.command()
+@click.option("--rss_feed", default=RSS_FEED, help="RSS feed to process.")
+@click.option(
+    "--gcs_bucket", default=GCS_BUCKET, help="GCS bucket in which to store data."
+)
+@click.option("--output", default="episodes.yaml", help="The output YAML file.")
+@click.option("--skip_processing", is_flag=True)
+def download(rss_feed, gcs_bucket, output, skip_processing):
+    episodes = []
+    feed = feedparser.parse(rss_feed)
+    for entry in feed.entries:
+        try:
+            episode = Episode(entry, gcs_bucket, skip_processing)
+            fulltext_url = episode.process()
+            episodes.append(fulltext_url)
+        except Exception as e:
+            console.print(f"[red]Error processing {entry.title} - skipping: {e}")
+    with open(output, "w") as outfile:
+        yaml.dump({"episodes": episodes}, outfile)
+    console.print(f"Processed {len(episodes)} episodes, Wrote output to {output}")
+
+
+if __name__ == "__main__":
+    download()
